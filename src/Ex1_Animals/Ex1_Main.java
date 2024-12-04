92aed1e89804caa9c295a3d8adb60c6b6f7a3075
r.link1 <r.link1@share.epsb.ca>
2024-12-04 11:31:56 -0700

    Start!

---
+++ src/Ex1_Animals/Ex1_Main.java
@@ -0,0 +1,16 @@
+package Ex1_Animals;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class Ex1_Main {
+    public static void run() {
+        ArrayList<Animal> allAnimals = new ArrayList<>();
+
+        allAnimals.add(new Animal("Peppy"));
+        allAnimals.add(new Dog("Scout"));
+        allAnimals.add(new Rotweiller("Rubble"));
+        allAnimals.add(new Shihtzu("Bubbles"));
+        allAnimals.add(new Trout("Slippey"));
+    }
+}
