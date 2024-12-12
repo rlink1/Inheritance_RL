efee7b08c8ba7c9e8652a371a4a359cc60a77edc
r.link1 <r.link1@share.epsb.ca>
2024-12-12 11:37:51 -0700

    Working on As5

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import Ex2_Restaurant.Restaurant_Main;
+import Ex3_MusicFestival.Ex3_Main;
 
 public class Main {
     public static void main(String[] args) {
 //        Ex1_Main.run();
-        Restaurant_Main.run();
+ //       Restaurant_Main.run();
+        Ex3_Main.run();
     }
 
 }
