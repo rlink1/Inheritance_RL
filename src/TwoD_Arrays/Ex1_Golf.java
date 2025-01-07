b4b73e9c7afbd78e2330d6ce154e95cede24089b
r.link1 <r.link1@share.epsb.ca>
2025-01-07 11:39:54 -0700

    start 2d arrays, still working on A5

---
+++ src/TwoD_Arrays/Ex1_Golf.java
@@ -0,0 +1,11 @@
+package TwoD_Arrays;
+
+public class Ex1_Golf {
+    public static void run() {
+        String[] names ={"Joe", "Serena", "Gill", "Stu"};
+
+        int[][] scorecard = new int[4][18];
+    }
+
+
+}
