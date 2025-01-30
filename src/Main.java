30bb54b817cded33594a513ab75d7b1f6a1f5352
r.link1 <r.link1@share.epsb.ca>
2025-01-30 11:40:49 -0700

    Work on As1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 import Ex2_Restaurant.Restaurant_Main;
 import Ex3_MusicFestival.Ex3_Main;
+import TwoD_Arrays.As1_Bingo;
 import TwoD_Arrays.Ex1_Golf;
 import TwoD_Arrays.Ex2_TicTacToegivemeanxgivemeanogivemethreeinarow;
+import TwoD_Arrays.Ex3_Sudoku;
 
 public class Main {
     public static void main(String[] args) {
 //        Ex1_Main.run();
  //       Restaurant_Main.run();
-        Ex3_Main.run();
+//        Ex3_Main.run();
 //        Ex1_Golf.run();
 //        Ex2_TicTacToegivemeanxgivemeanogivemethreeinarow.run();
+//        Ex3_Sudoku.run();
+        As1_Bingo.run();
     }
 
 }
