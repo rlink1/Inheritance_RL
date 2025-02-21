9a15d65f64a90357f9b9261b237157b62d5d3877
heytl <heytlia2929@gmail.com>
2025-02-20 20:42:55 -0700

    DONE AS5!!!

--- src/TwoD_Arrays/As5_Vigenere.java
+++ src/TwoD_Arrays/As5_Vigenere.java
@@ -2,19 +2,30 @@ package TwoD_Arrays;
 
 import Utilities.Library;
 
+import java.util.Arrays;
+
 public class As5_Vigenere {
     public static void run() {
-        char[] alphabet = {'A','B','C','D','E','F','G','H','I','J','K',
-                'L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-        char[][] vigenere =createVSquare(alphabet);
-        printSquare(vigenere);
+        char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+                'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        char[][] vigenere = createVSquare(alphabet);
+        Boolean mama = true;
+        while(mama) {
+            printSquare(vigenere);
+            System.out.println("What is the message you would like to decode? (Please use all capitals)");
+            String decoder= Library.input.nextLine();
+            System.out.println("What is the word you want to use to decode " + decoder + "? (Please use all capitals)");
+            String keyword = Library.input.nextLine();
+            System.out.println("Your decrypted message is:");
+            System.out.println(deCode(vigenere, decoder, keyword));
 
-        String keyword = "SCONA";
-
-        System.out.println(deCode(vigenere, "OGOGTSEYNTVCKA", keyword));
-
-
-
+            System.out.println("\nWould you like to continue?");
+            String choice = Library.input.nextLine();
+            if(choice.startsWith("n")){
+                mama = false;
+            }
+        }
+        System.out.println("Thank you for playing!");
     }
 
     private static char[][] createVSquare(char[] alphabet) {
@@ -23,11 +34,10 @@ public class As5_Vigenere {
 
         for (int r = 0; r < 26; r++) {
             for (int c = 0; c < square[r].length; c++) {
-                if(r+c <= 25){
-                    square[r][c] = alphabet[r+c];
-                }
-                else if(r+c > 25){
-                    square[r][c] = alphabet[(r+c) - 26];
+                if (r + c <= 25) {
+                    square[r][c] = alphabet[r + c];
+                } else if (r + c > 25) {
+                    square[r][c] = alphabet[(r + c) - 26];
                 }
 
             }
@@ -53,53 +63,50 @@ public class As5_Vigenere {
 
     }//end printSquare
 
-    public static int linearSearch (char[] arr, char searchTerm){
-        for(int i=0;i<arr.length;i++){
-            if(arr[i] == searchTerm){
+    public static int linearSearch(char[] arr, char searchTerm) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == searchTerm) {
                 return i;
             }
         }
         return -1;
     }
 
-    public static char getChar(char[][] arr, int row, int col){
+    public static char getChar(char[][] arr, int row, int col) {
         return arr[row][col];
     }
 
-    public static String[] deCode(char[][] arr, String m, String key){
-        char [] keyWord = key.toCharArray();
-        char [] message = m.toCharArray();
-        char [] exp = new char[message.length];
-        char [] e = new char[message.length];
-        String [] decoded = new String[message.length];
+    public static char[] deCode(char[][] arr, String m, String key) {
+        char[] keyWord = key.toCharArray();
+        char[] message = m.toCharArray();
+        char[] exp = new char[message.length];
+        char[] e = new char[message.length];
+        char[] decoded = new char[message.length];
         for (int i = 0; i < message.length; i++) {
-            if(i > keyWord.length - 1 ){
-                exp[i] = keyWord[i%keyWord.length];
-            }
-            else{
+            if (i > keyWord.length - 1) {
+                exp[i] = keyWord[i % keyWord.length];
+            } else {
                 exp[i] = keyWord[i];
             }
         }
 
-        for (int x = 0; x < message.length; x++) {
-            for (int j = 0; j < exp.length; j++) {
-                for (int i = 0; i < 26; i++) {
-                    if(arr[i].equals(exp[j])){
-                        for (int l = 0; l < message.length; l++) {
-                            int tempi = linearSearch(arr[0], message[l]);
-                            if(tempi > -1){
-                                decoded[x] = ((getChar(arr, i, tempi)) + ",");
+        for (int i = 0; i < exp.length; i++) {
+            for (int j = 0; j < 26; j++) { // used for row
+                if (exp[i] == arr[j][0]) {
+                        for (int k = 0; k < 26; k++) {
+                            if(arr[0][k] == message[i]){
+                                decoded[i] = arr[j][k];
                             }
                         }
 
-                    }
+
                 }
+
             }
         }
-            return decoded;
-        }
 
+        return decoded;
     }
 
-//end class
+} //end class
 
