7f8ba801b55eeac6ac3a175518c1342d182f4cdd
r.link1 <r.link1@share.epsb.ca>
2025-03-17 11:40:51 -0600

    Warking on fam tree

--- src/FamilyTree/FamilyTreeMain.java
+++ src/FamilyTree/FamilyTreeMain.java
@@ -1,5 +1,7 @@
 package FamilyTree;
 
+import Utilities.Library;
+
 import java.util.ArrayList;
 
 public class FamilyTreeMain {
@@ -19,7 +21,7 @@ public class FamilyTreeMain {
         person1.children.get(1).addChild(new Person("Avaya", "Germany"));
         person1.children.get(1).children.get(0).addChild(new Person("Ryza", "Japan"));
         person1.children.get(1).children.get(1).addChild(new Person("Newayna", "Germany"));
-        person1.children.get(1).children.get(0).children.get(0).addChild(new Person("Linkey Wink", "Australia"));
+        person1.children.get(1).children.get(0).children.get(0).addChild(new Person("Atticus", "Canada"));
 
 
 
@@ -29,23 +31,28 @@ public class FamilyTreeMain {
         person1.children.get(2).children.get(1).addChild(new Person("Zaiesah", "Russia"));
         person1.children.get(2).children.get(1).addChild(new Person("Steve", "Canada"));
 
-        countFamily(person1);
+        System.out.println(countFamily(person1) + 1);
         printFamily(person1);
+        System.out.println("\nThese people are LOWK canadian:");
+        printCanadians(person1);
+
+        printFromHere(person1);
     }
 
     public static int countFamily (Person ptemp){
+        int num = 0;
         if(ptemp.children.size() == 0){
-            return 0;
+            num += 0;
+
         }
         else{
-
             for (int i = 0; i < ptemp.children.size(); i++) {
-                return 1 + countFamily(ptemp.children.get(i));
+                num+= (1 + countFamily(ptemp.children.get(i)));
             }
 
 
         }
-return 0;
+return num;
 
 
     }
@@ -63,6 +70,65 @@ return 0;
         }
 
 
+    }
+
+    public static void printCanadians(Person ptemp){
+        if(ptemp.getCountry().equalsIgnoreCase("canada")){
+            System.out.println(ptemp.getName());
+            for (int i = 0; i < ptemp.children.size(); i++) {
+                printCanadians(ptemp.children.get(i));
+            }
+        }
+        else{
+            for (int i = 0; i < ptemp.children.size(); i++) {
+                printCanadians(ptemp.children.get(i));
+            }
+        }
+    }
+
+    public static void printFromHere(Person ptemp){
+        ArrayList<Integer> code = new ArrayList<>();
+        System.out.println("Who would you like to search for?");
+        String search = Library.input.nextLine();
+        if(checkChildren(ptemp, search, code) == -1){
+            System.out.println("Not found. Please try again");
+        }
+        else {
+            if(code.size() == 1){
+                System.out.println(ptemp.children.get(code.get(0)).getName());
+            }
+            else if(code.size() == 2){
+                System.out.println(ptemp.children.get(code.get(1)).children.get(code.get(0)).getName());
+            }
+            else if(code.size() == 3){
+                System.out.println(ptemp.children.get(code.get(2)).children.get(code.get(1)).children.get(code.get(0)).getName());
+            }
+            else if(code.size() == 4){
+                System.out.println(ptemp.children.get(code.get(3)).children.get(code.get(2)).children.get(code.get(1)).children.get(code.get(0)).getName());
+            }
+            else if(code.size() == 5){
+                System.out.println(ptemp.children.get(code.get(4)).children.get(code.get(3)).children.get(code.get(2)).children.get(code.get(1)).children.get(code.get(0)).getName());
+            }
+        }
+
+
+    }
+    public static int checkChildren(Person ptemp, String searchTerm, ArrayList<Integer> arr){
+        if(ptemp.getName().equalsIgnoreCase(searchTerm)){
+            System.out.println(ptemp.getName());
+            return 1;
+        }
+        else{
+            for (int i = 0; i < ptemp.children.size(); i++) {
+                if(checkChildren(ptemp.children.get(i), searchTerm, arr) > 0){
+                    arr.add(i);
+                    //print names; make it not a public static int
+                    return i;
+                }
+
+
+            }
+        }
     }
 
 }
--- src/FamilyTree/Person.java
+++ src/FamilyTree/Person.java
@@ -21,4 +21,8 @@ public class Person {
     public String getName() {
         return name;
     }
+
+    public String getCountry() {
+        return country;
+    }
 }//class
